3be781f4cfd5a04c04d1d347602d811bfe33abef
Mark Oberemk <moberemk@gmail.com>
2014-07-07 13:47:12 -0400

    Adding initial Vagrantfile

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+apt-get update
+
+# The oddly-named python-software-properties includes add-apt-repository.
+APT_PACKAGES="
+python-pip
+python-dev
+libcurl4-openssl-dev
+python-software-properties
+"
+
+apt-get -y install $APT_PACKAGES
+
+PIP_PACKAGES="
+futures
+pycurl
+tox
+twisted
+virtualenv
+"
+
+pip install $PIP_PACKAGES
+
+# TODO Install Postgres here
